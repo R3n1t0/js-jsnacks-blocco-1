78a10f41063f1c66c4c9ed5cfadec7bf85bf48d5
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-04-12 15:00:31 +0200

    jsnack-1 fatto

---
+++ jsnack1/script.js
@@ -0,0 +1,15 @@
+const primoNumero = prompt("Inserire il Primo Numero")
+const secondoNumero = prompt("Inserire il Secondo Numero")
+const contenuto = document.getElementById("contenuto")
+
+
+console.log(primoNumero);
+console.log(secondoNumero);
+
+if(primoNumero > secondoNumero){
+  contenuto.innerHTML = primoNumero;
+}else if(primoNumero < secondoNumero){
+  contenuto.innerHTML = secondoNumero;
+}else{
+  contenuto.innerHTML = PARI;
+}
